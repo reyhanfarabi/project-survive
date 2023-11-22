f72b6174f3b105dc3a027cc545b4fc7caf6de957
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2023-11-22 21:45:59 +0700

    add AI movement to enemy node

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -2,13 +2,29 @@ extends Area2D
 
 
 @export var entity: Resource_Entity
+@export var speed: int = 50
+
+@onready var player = get_parent().get_node("Player")
+
+var direction: Vector2
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	direction = Vector2(player.position - position).normalized()
+	handle_movement(delta)
+	handle_facing()
+
+
+func handle_movement(delta):
+	position += direction * speed * delta
+
+
+func handle_facing():
+	if direction.x > 0:
+		$Sprite2D.flip_h = false
+	elif direction.x < 0:
+		$Sprite2D.flip_h = true
