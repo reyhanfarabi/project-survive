2cd0bb684c68aa960c9d8dc5354e5c4aa0f84d69
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2023-11-27 17:18:18 +0700

    refactor enemy resource

---
+++ scripts/resources/resource_enemy.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name Resource_Enemy
+
+# enemy data
+@export var max_health: int
+@export var health: int
+@export var attack_damage: int
+@export var move_speed: int = 50
+@export var attack_cooldown: int = 2
+@export var exp_point: int
