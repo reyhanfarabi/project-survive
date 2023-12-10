990b1ab7b4b33a1ea9c408b7c19620b23c965bf7
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2023-12-10 22:11:56 +0700

    refactor enemy attack logic

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,41 +6,28 @@ extends Area2D
 @export var _destroy_comp: DestroyComponent
 @export var _take_damage_comp: TakeDamageComponent
 @export var _auto_move_comp: AutoMoveComponent
+@export var _enemy_attack_comp: EnemyAttackComponent
 
 @onready var _player = get_node("../../Player")
 @onready var _drops_container = get_node("../../DropsContainer")
-
-var _can_attack: bool = true
 
 
 func set_resource(value: Resource_Enemy):
 	_resource = value
 	_take_damage_comp.set_resource(value)
+	_enemy_attack_comp.set_resource(_resource)
 
 
-func _ready():
-	_start()
-
-
-func _start():
+func _ready() -> void:
 	$Sprite2D.texture = _resource.sprite
-	$AttackTimer.wait_time = _resource.attack_cooldown
+	_enemy_attack_comp.set_player_comp(_player)
 
 
 func _process(delta):
 	if not _player:
 		return
-	_handle_attack_to_player()
 	_destroy_comp.handle_destroy(_resource.health)
 	_auto_move_comp.move_towards_position(_player.position, _resource.move_speed, delta)
-
-
-func _handle_attack_to_player():
-	if not _can_attack or not overlaps_body(_player):
-		return
-	_can_attack = false
-	$AttackTimer.start()
-	_player.take_damage(_resource.attack_damage)
 
 
 func _spawn_exp_drop():
@@ -52,10 +39,6 @@ func _spawn_exp_drop():
 
 func take_damage(damage_taken: int) -> void:
 	_take_damage_comp.take_damage(damage_taken)
-
-
-func _on_attack_timer_timeout():
-	_can_attack = true
 
 
 func _on_destroy_component_trigger_side_effect() -> void:
