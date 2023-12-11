29bea534cf1b4346875305779bd4e4ec233b0cc1
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2023-12-11 14:02:10 +0700

    refactor spawn drop logic

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,11 +2,11 @@ extends Area2D
 
 
 @export var _resource: Resource_Enemy: set = set_resource
-@export var _exp_drop_scene: PackedScene
 @export var _destroy_comp: DestroyComponent
 @export var _take_damage_comp: TakeDamageComponent
 @export var _auto_move_comp: AutoMoveComponent
 @export var _enemy_attack_comp: EnemyAttackComponent
+@export var _exp_drop_comp: SpawnDropComponent
 
 @onready var _player = get_node("../../Player")
 @onready var _drops_container = get_node("../../DropsContainer")
@@ -21,6 +21,7 @@ func set_resource(value: Resource_Enemy):
 func _ready() -> void:
 	$Sprite2D.texture = _resource.sprite
 	_enemy_attack_comp.set_player_comp(_player)
+	_exp_drop_comp.set_drop_container(_drops_container)
 
 
 func _process(delta):
@@ -30,16 +31,13 @@ func _process(delta):
 	_auto_move_comp.move_towards_position(_player.position, _resource.move_speed, delta)
 
 
-func _spawn_exp_drop():
-	var e = _exp_drop_scene.instantiate()
-	e.position = position
-	e.exp_point = _resource.exp_point
-	_drops_container.add_child(e)
-
-
 func take_damage(damage_taken: int) -> void:
 	_take_damage_comp.take_damage(damage_taken)
 
 
 func _on_destroy_component_trigger_side_effect() -> void:
-	_spawn_exp_drop()
+	_exp_drop_comp.spawn()
+
+
+func _on_spawn_drop_component_trigger_additional_config(drop_scene) -> void:
+	drop_scene.exp_point = _resource.exp_point
